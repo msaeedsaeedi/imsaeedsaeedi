3a10447f39ce1bb901e984528bfa70b367dbecbd
Mohammad Saeed Saeedi <msaeedsaeedi@outlook.com>
2025-09-07 21:44:45 +0500

    feat: Skills added

--- src/app/sections/skills/skills.ts
+++ src/app/sections/skills/skills.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { SlideItem, Slider } from '../../components/slider/slider';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [Slider],
   templateUrl: './skills.html',
-  styleUrl: './skills.css'
+  styleUrl: './skills.css',
 })
 export class Skills {
-
+  readonly skills = signal<SlideItem[]>([
+    {
+      title: 'Angular',
+      description: 'TypeScript, RxJS, NgRx, PrimeNg, i18n, Unit Testing, Turborepo, Tailwind',
+    },
+    {
+      title: 'Backend',
+      description: 'Node.js, Express, MongoDB, PostgreSQL, Redis, JWT, REST APIs, GraphQL',
+    },
+    {
+      title: 'DevOps',
+      description: 'Docker, AWS, CI/CD, GitHub Actions, Nginx, Linux, Monitoring',
+    },
+    {
+      title: 'Tools',
+      description:
+        'Git, GitHub, VS Code, Figma, Jira, Postman, Chrome DevTools, Slack, Notion, Trello, Zoom',
+    },
+  ]);
 }
