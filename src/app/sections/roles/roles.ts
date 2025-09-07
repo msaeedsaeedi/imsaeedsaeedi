518ed9fd5517de6dd0a935b86bde3027724eb86f
Mohammad Saeed Saeedi <msaeedsaeedi@outlook.com>
2025-09-07 21:43:04 +0500

    feat: roles added

--- src/app/sections/roles/roles.ts
+++ src/app/sections/roles/roles.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { SlideItem, Slider } from '../../components/slider/slider';
 
 @Component({
   selector: 'app-roles',
-  imports: [],
+  imports: [Slider],
   templateUrl: './roles.html',
-  styleUrl: './roles.css'
+  styleUrl: './roles.css',
 })
 export class Roles {
-
+  readonly roles = signal<SlideItem[]>([
+    {
+      title: 'Project Manager',
+      description: 'Building SaaS MVPs that Scale',
+    },
+    {
+      title: 'Software Engineer',
+      description: 'Crafting Digital Solutions',
+    },
+    {
+      title: 'Product Owner',
+      description: 'Turning Ideas into Reality',
+    },
+  ]);
 }
