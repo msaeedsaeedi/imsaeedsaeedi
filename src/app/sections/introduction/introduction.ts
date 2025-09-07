06cac2558f7a95df7e4d2e244b9f5f963c8e917c
Mohammad Saeed Saeedi <msaeedsaeedi@outlook.com>
2025-09-07 21:41:33 +0500

    feat: introduction added

--- src/app/sections/introduction/introduction.ts
+++ src/app/sections/introduction/introduction.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-introduction',
   imports: [],
   templateUrl: './introduction.html',
-  styleUrl: './introduction.css'
+  styleUrl: './introduction.css',
 })
 export class Introduction {
-
+  protected readonly title = signal('I am Mohammad Saeed');
+  protected readonly subtitle = signal('Coder, Poet, and Dreamer');
 }
